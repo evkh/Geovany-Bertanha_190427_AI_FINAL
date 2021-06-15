a3ea3c98c8e98a7b99c51e9731aaf0178bf7ebad
evkh <gijow@live.com>
2021-06-15 00:05:55 -0300

    !!!!

---
+++ New Unity Project/Assets/AI.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+using System.Numerics;
+
+public class AI : MonoBehaviour
+{
+    // declarando variaveis
+    public Transform player;
+    public Transform bulletSpawn;
+    public Slider healthBar;   
+    public GameObject bulletPrefab;
+
+    NavMeshAgent agent;
+    public Vector3 destination; // The movement destination.
+    public Vector3 target;      // The position to aim to.
+    float health = 100.0f;
+    float rotSpeed = 5.0f;
+
+    float visibleRange = 80.0f;
+    float shotRange = 40.0f;
+
+    //pega o agent. se tiver com <100 de vida regenera
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = shotRange - 5; //for a little buffer
+        InvokeRepeating("UpdateHealth",5,0.5f);
+    }
+
+    //deixa a barra de vida visivel
+    void Update()
+    {
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        healthBar.value = (int)health;
+        healthBar.transform.position = healthBarPos + new Vector3(0,60,0);
+    }
+
+    //metodo que regenera a vida
+    void UpdateHealth()
+    {
+       if(health < 100)
+        health ++;
+    }
+    
+
+    //se o tiro acertar da dano
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "bullet")
+        {
+            health -= 10;
+        }
+    }
+
+    //faz o wonder
+    [Task]
+    public void PickRamdomDestination()
+    {
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        agent.SetDestination(dest);
+        Task.current.Succeed();
+
+    }
+    // pega uma posição pra andar
+    [Task]
+    public void PickDestination(int x , int y)
+    {
+        Vector3 dest = new Vector3(x, 0, y);
+        agent.SetDestination(dest);
+        Task.current.Succeed();
+    }
+
+    //se move para determinada posição
+    [Task]
+    public void MoveToDestination()
+    {
+        if (Task.isInspected)
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            Task.current.Succeed();
+        }
+    }
+
+    //pega a posição do player
+    [Task]
+    public void TargetPlayer()
+    {
+        target = player.transform.position;
+        Task.current.Succeed();
+    }
+    //Atira
+    [Task]
+    public bool Fire()
+    {
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 2000);
+
+        return true;
+    }
+
+}
+
---
+++ New Unity Project/Assets/Drive.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Drive : MonoBehaviour {
+    // declarando variaveis
+	float speed = 20.0F;
+    float rotationSpeed = 120.0F;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    float health = 100.0f;
+    public Slider healthBar;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateHealth", 5, 0.5f);
+    }
+    //faz o player se mover por translate
+    void Update() {
+       
+        
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+       
+      
+        
+        //atira ao apertar Space
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000);
+        }
+
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        healthBar.value = (int)health;
+        healthBar.transform.position = healthBarPos + new Vector3(0, 30, 0);
+    }
+
+    void UpdateHealth()
+    {
+        if (health < 100)
+            health++;
+    }
+}
